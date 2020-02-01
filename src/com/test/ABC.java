6f2d08feb133ecc591700e82294cd902e2e2d499
Nitin <Nitin@LAPTOP-91DMNFDN>
2020-02-01 19:28:50 +0530

    class

--- src/com/test/ABC.java
+++ src/com/test/ABC.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class ABC {
 	public static void main(String[] args) {
 		System.out.println("ABC");
+		//
 		List<Integer> ns=new ArrayList<Integer>();
 		ns.add(10);
 		ns.add(1);
